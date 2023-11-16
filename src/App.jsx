cf0bb55d3a4c14ff384e29909bb8b778d8211497
Patryk <krasowski.p@outlook.com>
2023-11-16 20:15:03 +0100

    transfer gameBoard logic from gameBoard to App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import { useState } from 'react'
 import GameBoard from './components/GameBoard'
 import Player from './components/Player'
 import Log from './components/Log'
+import { WINNING_COMBINATIONS } from './winning-combinations.js'
+const initialGameBoard = [
+	[null, null, null],
+	[null, null, null],
+	[null, null, null],
+]
 
 const deriveActivePlayer = gameTurns => {
 	let currentPlayer = 'X'
@@ -16,6 +22,20 @@ function App() {
 
 	const activePlayer = deriveActivePlayer(gameTurns)
 
+	let gameBoard = initialGameBoard
+
+	for (const turn of gameTurns) {
+		const { square, player } = turn
+		const { row, col } = square
+		gameBoard[row][col] = player
+	}
+
+	for(const combination of WINNING_COMBINATIONS){
+		const firstSquareCombination = gameBoard
+		const secondSquareCombination = gameBoard
+		const thirdSquareCombination = gameBoard
+	}
+	
 	const selectSquareHandler = (rowIndex, colIndex) => {
 		setGameTurns(prevTurns => {
 			const currentPlayer = deriveActivePlayer(prevTurns)
@@ -30,7 +50,7 @@ function App() {
 					<Player initalName='Player 1' symbol='X' isActive={activePlayer === 'X'}></Player>
 					<Player initalName='Player 2' symbol='O' isActive={activePlayer === 'O'}></Player>
 				</ol>
-				<GameBoard onSelectSquare={selectSquareHandler} turns={gameTurns} />
+				<GameBoard onSelectSquare={selectSquareHandler} board={gameBoard} />
 			</div>
 			<Log turns={gameTurns} />
 		</main>
